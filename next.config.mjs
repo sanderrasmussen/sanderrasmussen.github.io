37035b287505b69026c3d3974ece1fd33a35243f
Sander Rasmussen <sander.rasmussn@gmail.com>
2024-06-13 23:16:10 +0200

    m

--- next.config.mjs
+++ next.config.mjs
@@ -4,10 +4,31 @@
 export default nextConfig;
 */ 
 
+/** @type {import('next').NextConfig} */
 const nextConfig = {
-    basePath: "/nextjs-github-pages",
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
+    /**
+     * Enable static exports for the App Router.
+     *
+     * @see https://nextjs.org/docs/app/building-your-application/deploying/static-exports
+     */
+    output: "export",
+  
+    /**
+     * Set base path. This is the slug of your GitHub repository.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
+     */
+    basePath: "/sanderrasmussn.github.io",
+  
+    /**
+     * Disable server-based image optimization. Next.js does not support
+     * dynamic features with static exports.
+     *
+     * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
+     */
+    images: {
+      unoptimized: true,
+    },
   };
   
-  module.exports = nextConfig; 
+  export default nextConfig;
